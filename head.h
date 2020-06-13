159a0a08085dbb21d4cda8828cce430c54dbec3e
jasfa <60039036+jasfa@users.noreply.github.com>
2020-06-13 22:18:00 +0800

    Update head.h

--- head.h
+++ head.h
@@ -7,3 +7,13 @@
 #include<windows.h>
 #include<winuser.h> 
 using namespace std;
+int block[7][4][4][4];//block[方块种类][4种变形][][] 
+#define N 100
+int map1[N][N]={0};
+int map2[N][N]={0}; 
+string name1, name2; 
+int gg1=0,gg2=0,score1=0,score2=0;
+void gotoxy(int x, int y) {//gotoxy（x，y）将光标移动到指定行y和列x
+	COORD pos = { x,y };
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);//两个参数分别是指定哪个窗体，具体位置
+}
